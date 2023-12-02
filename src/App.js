c36bc6cceb325ffdf6f59aa789369f1a32b1efef
evan3v4n <evan.scott2050@Gmail.com>
2023-12-02 16:23:27 -0500

    Initial React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <header/>
+    </div>
+  );
+}
+
+export default App;
