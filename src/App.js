701f42b612d25742b5cb44937410bb6125217845
SaaraUthmaan <149133492+SaaraUthmaan@users.noreply.github.com>
2023-12-03 08:41:11 -0800

    Update App.js
    changed it to browser instead of 
    function App() { 
    return ( <main> <HomePage /> <AboutPage /> <MapComponent /> </main> ) }

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MapComponent from './components/MapComponent';
 import HomePage from './components/HomePage';
+import HomePage from './components/AboutPage';
 
 function App() {
-  return (
-    <main>
-      <HomePage />
-      <AboutPage />
-      <MapComponent />
-    </main>
-  )
+ return (
+    <BrowserRouter>
+      <Routes>  
+        <Route exact path="/" element={<Homepage/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
+
+
+
+  
